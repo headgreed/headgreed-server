e5bd4afe8fc7dcc3fd2e72928f75fec2d4b4af64
Hsu <edittye379@gmail.com>
2016-12-26 07:23:18 +0800

    users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('ori_name');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('avatar');
+            $table->boolean('gender');
+            $table->string('slug');
+            $table->string('location')->nullable();
+            $table->text('about')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
