68abbac8e7b107170e4ee75fb1aad5be62ac67dd
Hsu <edittye379@gmail.com>
2017-01-07 16:26:02 +0800

    :package: refactor facebook

--- app/Http/Controllers/Auth/SocialAuthController.php
+++ app/Http/Controllers/Auth/SocialAuthController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 use Socialite;
 use App\SocialAccount;
 use App\User;
-use GuzzleHttp\Client;
 
 class SocialAuthController extends Controller
 {
@@ -18,7 +17,19 @@ class SocialAuthController extends Controller
     public function callback()
     {
         $data = Socialite::driver('facebook')->user();
+        $user = $this->facebookService($data);
+        auth()->login($user);
+        return redirect('/home#');
+    }
 
+    public function facebook(Request $request)
+    {
+        $user = $this->facebookService($request);
+        return $user;
+    }
+
+    public function facebookService($data)
+    {
         $account = SocialAccount::whereProvider('facebook')
             ->whereProviderUserId($data->id)
             ->first();
@@ -48,62 +59,18 @@ class SocialAuthController extends Controller
                 $new_path = storage_path("app/public/avatar/{$new_name}");
                 file_put_contents($new_path, $file);
 
+                if (isset($data->gender)) {
+                    $gender = $data->gender;
+                } else {
+                    $gender = $data->user['gender'];
+                }
+
                 $user = User::create([
                     'slug' => uniqid(),
                     'ori_name' => $data->name,
                     'name' => $data->name,
                     'email' => $email,
-                    'gender' => $data->user['gender'],
-                    'avatar' => $new_name,
-                    'api_token' => md5(uniqid(rand(), TRUE))
-                ]);
-            }
-
-            $account->user()->associate($user);
-            $account->save();
-        }
-
-        auth()->login($user);
-        return redirect('/home#');
-    }
-
-    public function facebook(Request $request)
-    {
-        $account = SocialAccount::whereProvider('facebook')
-            ->whereProviderUserId($request->id)
-            ->first();
-
-        if ($account) {
-            $user = $account->user;
-        }
-        else {
-            $email = $request->email;
-            if ($email == null) {
-                $email = $request->id . "@fb.headgreed.com.tw";
-            }
-
-            $account = new SocialAccount([
-               'provider' => 'facebook',
-               'provider_user_id' => $request->id,
-               'avatar_url' => $request->avatar,
-               'avatar_original' => $request->avatar_original,
-               'profileUrl' => $request->profileUrl
-            ]);
-
-            $user = User::whereEmail($email)->first();
-
-            if (!$user) {
-                $file = file_get_contents($request->avatar);
-                $new_name = uniqid().".jpg";
-                $new_path = storage_path("app/public/avatar/{$new_name}");
-                file_put_contents($new_path, $file);
-
-                $user = User::create([
-                    'slug' => uniqid(),
-                    'ori_name' => $request->name,
-                    'name' => $request->name,
-                    'email' => $email,
-                    'gender' => $request->gender,
+                    'gender' => $gender,
                     'avatar' => $new_name,
                     'api_token' => md5(uniqid(rand(), TRUE))
                 ]);
