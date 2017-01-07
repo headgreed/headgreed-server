b7489f0850a26262af8b076c8d60ddbb1f0aee8a
Hsu <edittye379@gmail.com>
2017-01-08 02:44:28 +0800

    elixir: versioning css js file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,8 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="/css/app.css" rel="stylesheet">
+    {{-- <link href="/css/app.css" rel="stylesheet"> --}}
+    <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
 
     <!-- Scripts -->
     <script>
@@ -108,7 +109,8 @@
     </div>
 
     <!-- Scripts -->
-    <script src="/js/app.js"></script>
+    {{-- <script src="/js/app.js"></script> --}}
+    <script src="{{ elixir('js/app.js') }}"></script>
     @yield('js')
 </body>
 </html>
